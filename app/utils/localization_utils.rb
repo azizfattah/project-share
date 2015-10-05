991ca8b82aa7d380e046b7987b947a0addda0f0c
Bibek Sharma <bipashant@gmail.com>
2015-10-05 11:27:06 +0545

    paypal_integration Paypal Integration on admin side completed

--- app/utils/localization_utils.rb
+++ app/utils/localization_utils.rb
@@ -4,7 +4,7 @@ module LocalizationUtils
   # Check if the country code (ISO 3166-1 alpha-2) is valid (i.e. sv_SE -> SE is the country code)
   def country_code_valid?(country_code)
     # unless country_code.blank?
-    #   binding.pry
+    #
     #   CountrySelect.countries.select{|key, hash|
     #     return true if key.downcase == country_code.downcase
     #   }
